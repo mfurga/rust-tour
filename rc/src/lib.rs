fa125727ab2c4301b9a6f7ac67b0c60a1c7bcd42
Mateusz Furga <matfurga@gmail.com>
2025-11-16 21:08:13 +0100

    update rc

--- rc/src/lib.rs
+++ rc/src/lib.rs
@@ -1,13 +1,13 @@
-use std::{cell::Cell, fmt::Debug, ops::Deref};
+use std::{cell::Cell, fmt::Debug, ops::Deref, ptr::NonNull};
 
 pub struct MyRc<T> {
-    inner: *const MyInner<T>,
+    inner: NonNull<MyInner<T>>,
     _marker: std::marker::PhantomData<MyInner<T>>,
 }
 
 impl<T: Debug> Debug for MyRc<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "MyRc ({:?})", unsafe { &*self.inner })
+        write!(f, "MyRc ({:?})", unsafe { self.inner.as_ref() } )
     }
 }
 
@@ -19,8 +19,14 @@ pub struct MyInner<T> {
 
 impl<T> MyRc<T> {
     pub fn new(value: T) -> Self {
+        let inner = Box::new(MyInner {
+            inner: value,
+            references: Cell::new(1)
+        });
+
         Self {
-            inner: Box::into_raw(Box::new(MyInner { inner: value, references: Cell::new(1) })),
+            // SAFETY: Box::into_raw never returns null
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
             _marker: std::marker::PhantomData
         }
     }
@@ -31,7 +37,7 @@ impl<T> Deref for MyRc<T> {
 
     fn deref(&self) -> &Self::Target {
         // SAFETY: we ensure that the pointer is valid as long as there is at least one MyRc<T> alive
-        let inner = unsafe { &*self.inner };
+        let inner = unsafe { self.inner.as_ref() };
         &inner.inner
     }
 }
@@ -39,7 +45,7 @@ impl<T> Deref for MyRc<T> {
 impl<T> Clone for MyRc<T> {
     fn clone(&self) -> Self {
         // SAFETY: we ensure that the pointer is valid as long as there is at least one MyRc<T> alive
-        let inner = unsafe { &*self.inner };
+        let inner = unsafe { self.inner.as_ref() };
         inner.references.set(inner.references.get() + 1);
 
         Self {
@@ -52,12 +58,12 @@ impl<T> Clone for MyRc<T> {
 impl<T> Drop for MyRc<T> {
     fn drop(&mut self) {
         // SAFETY: we ensure that the pointer is valid as long as there is at least one MyRc<T> alive
-        let inner = unsafe { &*self.inner };
+        let inner = unsafe { self.inner.as_ref() };
         inner.references.set(inner.references.get() - 1);
 
         if inner.references.get() == 0 {
             // SAFETY: we ensure that the pointer is valid as long as there is at least one MyRc<T> alived
-            let _ = unsafe { Box::from_raw(self.inner as *mut MyInner<T>) };
+            let _ = unsafe { Box::from_raw(self.inner.as_ptr()) };
         }
 
     }
